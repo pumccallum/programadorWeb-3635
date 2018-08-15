9abb8f3ef4ccd5f8041dcd617fdef4ac66cc5077
pumccallum <punu.m@hotmail.com>
2018-08-14 21:30:57 -0300

    Ejercicio clase 1

---
+++ Clase 01/Alumno/Ejercicio/js/index.js
@@ -0,0 +1,40 @@
+//1
+
+// var nombre = prompt('Ingresá tu nombre')
+
+// var mensaje = 'Hola ' + nombre +'!'
+
+// console.log(mensaje)
+
+
+//2
+
+// var numero1 = prompt('Ingresá un número')
+
+// var parse1 = parseFloat(numero1, 10)
+
+// var numero2 = 8
+
+// console.log(parse1 + numero2)
+
+//3
+
+// var numero3 = prompt('Ingresá un número')
+
+// var parse2 = parseFloat(numero3, 10)
+
+// var numero4 = 2
+
+// console.log(parse2 - numero4)
+
+//4
+
+var numero5 = prompt('Ingresá un número')
+
+var numero6 = prompt('Ingresá otro número')
+
+var parse3 = parseFloat(numero5, 10)
+
+var parse4 = parseFloat(numero6, 10)
+
+console.log(parse3 + parse4)
