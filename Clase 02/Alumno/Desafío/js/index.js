a17e3b2c0540bb580cb104297bb3f469c7cea06c
pumccallum <punu.m@hotmail.com>
2018-08-16 21:27:24 -0300

    Desafio 2

---
+++ Clase 02/Alumno/Desafío/js/index.js
@@ -0,0 +1,26 @@
+var gender = prompt('Indique su sexo (masculino, femenino, otro)')
+var age = prompt('Indique su eded')
+
+var message
+
+switch (gender) {
+  case 'masculino':
+    message = 'Sr.'
+    break
+  case 'femenino':
+    message = 'Sra.'
+    break
+  case 'otro':
+    message = 'Sx.'
+    break
+  default:
+    message = '(Sr./Sra./Sx.)'
+}
+
+if (age < 18) {
+  message = message + ' usted es menor de edad, no puede ingresar.'
+} else if (age >= 18) {
+  message = message + ' usted es mayor de edad, puede ingresar.'
+}
+
+console.log(message)
