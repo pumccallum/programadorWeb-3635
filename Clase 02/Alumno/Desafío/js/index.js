232cdcb13aaabdd38b4926988ef47386e6432a5b
pumccallum <punu.m@hotmail.com>
2018-08-16 21:34:48 -0300

    Desafio 2

--- Clase 02/Alumno/Desafío/js/index.js
+++ Clase 02/Alumno/Desafío/js/index.js
@@ -21,6 +21,7 @@ if (age < 18) {
   message = message + ' usted es menor de edad, no puede ingresar.'
 } else if (age >= 18) {
   message = message + ' usted es mayor de edad, puede ingresar.'
+} else {
+  message = message + ' su edad no ha sido ingresada correctamente'
 }
-
 console.log(message)
